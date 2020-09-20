e28776d046318152f06c4b5b9ffb8cfdbb38369b
Synyster <tarriestfilly98@gmail.com>
2020-09-20 13:48:50 +0700

    read from file working -> queue work

--- get_audio.py
+++ get_audio.py
@@ -6,7 +6,11 @@ import os
 
 
 def get_audio_from_url(url):    # Достаем аудио из ютуба
-    song = pafy.new(url).getbestaudio()
+    song = ''
+    if url[len(url)-1] == '\n':
+        song = pafy.new(url[:len(url)-1]).getbestaudio()
+    else:
+        song = pafy.new(url).getbestaudio()
     song.download()
     return song.filename
 
@@ -16,12 +20,38 @@ def convert(name):  # Конвертируем и экспортируем
     music.export('songs/' + name[:(len(name)-4)] + 'mp3', format="mp3")
 
 
+def file_worker():  # чтение из файла url
+    handler = open('queue.txt','r+')
+    buf = handler.readlines()
+    handler.seek(0) # Перемещаем указатель на 0 символ
+    return buf, handler
+
+
+def del_in_file(buf, del_line, handler): # Перезапись файла
+    for line in buf:
+        if line != del_line:
+            handler.write(line)
+    handler.truncate()
+    handler.seek(0)
+
+
 if __name__ == "__main__":
-    name = get_audio_from_url(sys.argv[1])
-    convert(name)
-    # далее удаление
-    path = os.path.join(os.path.abspath(os.path.dirname(__file__)), name)
-    os.remove(path)
+    buf, filee = file_worker()
+    counter = 0
+    for line in buf:
+        name = get_audio_from_url(line)
+        convert(name)
+
+        # далее удаление
+        path = os.path.join(os.path.abspath(os.path.dirname(__file__)), name)
+        os.remove(path)
+        del_in_file(buf, line, filee)
+        buf[counter] = ''
+        counter += 1
+    filee.close()
+        
+        
+
 
 
 # Добавить установку в директорию
